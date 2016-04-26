8d91f24ccb68c80520422d8bde25db8280bb3b67
Anthony <aiello83@hotmail.com>
2016-04-26 10:53:24 +0200

    First commiy

--- app/src/main/java/com/oklm/oklm/MainActivity.java
+++ app/src/main/java/com/oklm/oklm/MainActivity.java
@@ -1,13 +1,14 @@
 package com.oklm.oklm;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +19,48 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final TextView Chrono = (TextView)findViewById(R.id.textView);
+
+        new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                Chrono.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                Chrono.setText("done!");
+            }
+        }.start();
+
+        /*** Gestion du bouton GO***/
+        Button Go = (Button) findViewById(R.id.goBoutton);
+       //initialisation Timer
+       final CountDownTimer  Test=  new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                Chrono.setText("seconds remaining: " + millisUntilFinished / 1000);
+
+            }
+
+            public void onFinish() {
+                Chrono.setText("done!");
+            }
+        };
+
+
+        Go.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                Test.start();
+
+
+
             }
         });
+
+
+
     }
 
     @Override
